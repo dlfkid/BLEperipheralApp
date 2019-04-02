5628cb56c81633d740aeebf350cb94443a6afd24
Ivan_deng <dlfkid@icloud.com>
2019-04-02 23:37:18 +0800

    继续实现特征Cell

--- BLEDemoPeripheral/View/CentralCharacteristicTableViewCell.m
+++ BLEDemoPeripheral/View/CentralCharacteristicTableViewCell.m
@@ -13,6 +13,7 @@
 
 // Helpers
 #import <UIColor+UIExtensionKit.h>
+#import "CBCharacteristic+StringExtensions.h"
 
 @interface CentralCharacteristicTableViewCell()
 
@@ -29,15 +30,18 @@
     if (self) {
         _uuidLabel = [[UILabel alloc] initWithFrame:CGRectZero];
         _uuidLabel.font = [UIFont systemFontOfSize:[UIFont systemFontSize]];
+        _uuidLabel.text = NSLocalizedString(@"CharacteristicTableViewCell.UUID", "");
         [self.customContentView addSubview:self.uuidLabel];
         
         _valueLabel = [[UILabel alloc] initWithFrame:CGRectZero];
         _valueLabel.textColor = [UIColor tintColor];
+        _valueLabel.text = NSLocalizedString(@"CharacteristicTableViewCell.value", "");
         [self.customContentView addSubview:self.valueLabel];
         
         _propertiesLabel = [[UILabel alloc] initWithFrame:CGRectZero];
         _propertiesLabel.font = [UIFont systemFontOfSize:[UIFont systemFontSize]];
         _propertiesLabel.textColor = [UIColor brownColor];
+        _propertiesLabel.text = NSLocalizedString(@"CharacteristicTableViewCell.property", "");
         [self.customContentView addSubview:self.propertiesLabel];
         
     }
@@ -46,12 +50,35 @@
 
 - (void)updateConstraints {
     
+    [self.uuidLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_equalTo(10);
+        make.top.mas_equalTo(10);
+        make.right.mas_equalTo(-10);
+    }];
+    
+    [self.propertiesLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.bottom.mas_equalTo(-10);
+        make.left.mas_equalTo(10);
+    }];
+    
+    [self.valueLabel mas_remakeConstraints:^(MASConstraintMaker *make) {
+        make.right.mas_equalTo(-10);
+        make.centerY.mas_equalTo(0);
+    }];
+    
     [super updateConstraints];
 }
 
 
 - (void)setCharacter:(CBCharacteristic *)character {
     _character = character;
+    
+    NSString *properties = [CBCharacteristic propertiesString:character.properties];
+    self.propertiesLabel.text = [properties stringByAppendingFormat:@" %@", properties];
+}
+
++ (CGFloat)rowHeight {
+    return 90;
 }
 
 @end
